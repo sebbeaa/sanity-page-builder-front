765f57a5bd584b8944e04677722926803554129e
Sebastian Aanstad <sebbeaanstad@gmail.com>
2024-05-09 15:20:58 +0200

    updates

--- components/global/header/header.tsx
+++ components/global/header/header.tsx
@@ -1,12 +1,4 @@
-"use client";
-
-import { useEffect, useRef } from "react";
-
+"use server";
 export const HeaderNav = ({ headerContent }: { headerContent: any }) => {
-  const ref = useRef<any | null>(null);
-  useEffect(() => {
-    ref.current.innerHTML = headerContent;
-  }, [headerContent]);
-
-  return <header ref={ref} />;
+  return <div dangerouslySetInnerHTML={{ __html: headerContent }} />;
 };
